c4901149eb08f4119d59ae23a08c6dd5ef6c9f19
sadness_ojisan <sadness.ojisan@gmail.com>
2022-01-23 00:45:11 +0900

    fix clippy

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-#![allow(unused)]
+use colored::Colorize;
 
 use clap::Parser;
-use pasona::question::{Choice, Kind, Question};
-use std::{collections::HashMap, error::Error, fmt::Result, num::ParseIntError, slice::SliceIndex};
+use pasona::question::{Kind, Question};
+use std::collections::HashMap;
 
 /// あなたのパーソナルカラーを診断します
 #[derive(Parser)]
@@ -24,11 +24,59 @@ impl State {
             ]),
         }
     }
+    fn print_answer(self) {
+        let mut data_vec: Vec<_> = self.result.into_iter().collect();
+        data_vec.sort_by(|a, b| b.1.cmp(&a.1));
+        let max_answer = data_vec
+            .get(0)
+            .expect("データがありません。想定外のエラーです。");
+        println!();
+        println!();
+        println!();
+        println!();
+        println!();
+        match max_answer.0 {
+            Kind::イエベ春 => {
+                println!(
+                    "{}",
+                    "あなたは イエベ春 です。あなたに似合うライブラリは Svelte です。"
+                        .bright_yellow()
+                );
+                println!("{}", "https://svelte.jp/".red());
+            }
+            Kind::ブルベ夏 => {
+                println!(
+                    "{}",
+                    "あなたは ブルベ夏 です。あなたに似合うライブラリは React です。".cyan()
+                );
+                println!("{}", "https://ja.reactjs.org/".cyan());
+            }
+            Kind::イエベ秋 => {
+                println!(
+                    "{}",
+                    "あなたは イエベ秋 です。あなたに似合うライブラリは D3.js です。".yellow()
+                );
+                println!("{}", "https://d3js.org/".yellow());
+            }
+            Kind::ブルベ冬 => {
+                println!(
+                    "{}",
+                    "あなたは ブルベ冬 です。あなたに似合うライブラリは jQuery です。".blue()
+                );
+                println!("{}", "https://jquery.com/".blue());
+            }
+        }
+        println!();
+        println!();
+        println!();
+        println!();
+        println!();
+    }
 }
 
 fn main() {
     let mut state = State::new();
-    let args = Cli::parse();
+    Cli::parse();
     let questions = Question::new_questions();
 
     let mut loop_cnt = 0;
@@ -42,7 +90,7 @@ fn main() {
         let mut word = String::new();
         let input_result = std::io::stdin().read_line(&mut word);
         match input_result {
-            Ok(input) => {
+            Ok(_) => {
                 let answer = word.trim();
                 let parsed_result = answer.parse::<usize>();
                 match parsed_result {
@@ -50,21 +98,22 @@ fn main() {
                         let choice = question.choices.get(num);
                         match choice {
                             Some(choice) => {
-                                for (key, value) in choice.score.iter() {
+                                for (key, _) in choice.score.iter() {
                                     let current_value = state.result.get_mut(key).unwrap();
                                     *current_value += 1;
                                 }
-                                if (questions.len() == loop_cnt + 1) {
+                                if questions.len() == loop_cnt + 1 {
                                     break;
-                                }
+                                };
                                 loop_cnt += 1;
+                                println!();
                             }
                             None => {
                                 println!("範囲外の入力です。やり直してください。");
                             }
                         }
                     }
-                    Err(err) => {
+                    Err(_) => {
                         println!("無効な入力です。やり直してください。");
                     }
                 }
@@ -74,4 +123,5 @@ fn main() {
             }
         }
     }
+    state.print_answer();
 }
